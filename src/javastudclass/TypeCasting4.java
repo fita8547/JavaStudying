d160022baefda60afe509313afb4426b85195281
fita8547 <junsumon090608@gmail.com>
2026-03-04 15:12:24 +0900

    first commit

---
+++ src/javastudclass/TypeCasting4.java
@@ -0,0 +1,18 @@
+package javastudclass;
+
+public class TypeCasting4 {
+    public static void main(String[] args) {
+        byte value6 =(byte)128;
+        int value7 =(int)3.5;
+        float value8 = (float) 7.5;
+
+
+        System.out.printf(String.valueOf(value6));
+        System.out.printf(String.valueOf(value7));
+        System.out.printf(String.valueOf(value8));
+
+    }
+}
+
+
+//3얼 4일짜 변수와 자료형 캐스팅
