445809ef8bbef19afcc3d86cca87e507790a17f3
fita8547 <junsumon090608@dgsw.hs.kr>
2026-03-05 13:49:13 +0900

    feat:vaeiable.ex과제제출

---
+++ src/variable/ex/VarEx1Question.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx1Question {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 3;
+
+        System.out.printf(String.valueOf(num1+num2));
+        System.out.printf(String.valueOf(num1-num2));
+        System.out.printf(String.valueOf(num1*num2));
+    }
+}
---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,10 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long ddd = 1000000000000L;
+        System.out.printf(String.valueOf(ddd));
+        boolean sss= true;
+        System.out.printf(String.valueOf(sss));
+    }
+}
