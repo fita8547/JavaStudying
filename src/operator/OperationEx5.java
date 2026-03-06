6e12e74cb25cde478fa6690bf0eaa387b454be24
fita8547 <junsumon090608@gmail.com>
2026-03-06 09:37:18 +0900

    add:operationEx&Comp&&Assing

---
+++ src/operator/OperationEx5.java
@@ -0,0 +1,9 @@
+package operator;
+
+public class OperationEx5 {
+    public static void main(String[] args) {
+            int x= 1;
+            String result = x%2==0 ? "짝수":"홀수";
+            System.out.println("x ="+x+","+result);
+    }
+}
