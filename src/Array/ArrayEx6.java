42961ca4e3e3a3d3310f49d15e68a766ba378224
fita8547 <junsumon090608@dgsw.hs.kr>
2026-03-19 14:26:17 +0900

    Add:Array

--- src/Array/ArrayEx6.java
+++ src/Array/ArrayEx6.java
@@ -1,4 +1,37 @@
 package Array;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 public class ArrayEx6 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        System.out.print("입력받을 숫자의 개수를 입력하세요: ");
+        int num= Integer.parseInt((br.readLine()));
+        int[] arr = new int[num];
+        for (int i = 0; i < num; i++) {
+            System.out.print("정수를 입력하세요: ");
+            int a = Integer.parseInt(br.readLine());
+            arr[i] = a;
+        }
+        int max1=arr[0];
+        int min1=arr[0];
+        for (int i = 1; i < num; i++) {
+            if (max1 < arr[i]){
+                max1 = arr[i];
+            }
+            if (min1 > arr[i]){
+                min1 = arr[i];
+            }
+        }
+
+
+        System.out.print("가장 큰값");
+        System.out.println(max1);
+
+        System.out.print("가장 작은값");
+        System.out.println(min1);
+
+    }
 }
--- src/Array/ArrayEx8.java
+++ src/Array/ArrayEx8.java
@@ -1,4 +1,27 @@
 package Array;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 public class ArrayEx8 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] arr = new int[9][9];
+        for (int i=1; i<=9; i++){
+            for (int j=1; j<=9; j++){
+
+                arr[i-1][j-1] = i*j;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("[");
+            for (int j = 0; j < arr[i].length-1; j++) {
+                System.out.print(arr[i][j] + ",");
+            }
+            System.out.print(arr[i][8]+"]");
+            System.out.println();
+        }
+
+
+    }
 }
