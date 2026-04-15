1a0ff6aaf1a1ee8b08b80b8936c2a20f2a47980c
fita8547 <junsumon090608@gmail.com>
2026-04-15 15:19:38 +0900

    final1

---
+++ src/final1/ConstructInit.java
@@ -0,0 +1,12 @@
+package final1;
+
+public class ConstructInit {
+    public String name;
+    public int value;
+    public ConstructInit(int value){
+        this.value = value;
+    }
+    public int getValue(){
+        return this.value;
+    }
+}
---
+++ src/final1/FieldInit.java
@@ -0,0 +1,9 @@
+package final1;
+
+public class FieldInit {
+    public static final int CONST_VALUE = 10;
+    private final int value =10;
+    public int getValue(){
+        return this.value;
+    }
+}
